c36ae998d7e2628cd8df8c684f1fb01a42b6c65c
hsoboh <hussein.soboh@gmail.com>
2020-05-16 11:22:52 +0300

    scaled and not_scaled baselines

--- simulation/code/utils.R
+++ simulation/code/utils.R
@@ -1,38 +1,40 @@
 library("moments")#skewness and kurtosis
 
-calc_sample_features<-function(s){
-  scaled_s <- scale(s)
-  stats <- calc_stats(scaled_s)
-  #features <- stats[c("size", "mean", "median", "skewness", "kurtosis", "outliers_minor", "outliers_extream", )]
-  return(stats)
-}
-
-calc_stats<-function(scaled_s){
+calc_stats<-function(sample, scaled_sample = NULL){
+  if(do_scaling == TRUE){
+    if(is.null(scaled_sample)){
+      s <- scale(sample)   
+    }else{
+      s <- scaled_sample
+    }
+  }else{
+    s <- sample
+  }
   #Calc features
-  size <- length(scaled_s)
-  mean_ <- round(mean(scaled_s), 5)
-  median_ <- round(median(scaled_s),5)
-  sd_ <- sd(scaled_s)
-  skewness_ <- round(skewness(scaled_s), 5)
-  kurtosis_ <- round(kurtosis(scaled_s), 5)
-  outliers <- find_outliers(scaled_s)
+  size <- length(s)
+  mean_ <- round(mean(s), 5)
+  median_ <- round(median(s),5)
+  sd_ <- sd(s)
+  skewness_ <- round(skewness(s), 5)
+  kurtosis_ <- round(kurtosis(s), 5)
+  outliers <- find_outliers(s)
   
-  sigma_1_ratio <- length(which(abs(scaled_s) <= 1 ))/size
-  sigma_2_ratio <- length(which(abs(scaled_s) <= 2 ))/size
-  sigma_3_ratio <- length(which(abs(scaled_s) <= 3 ))/size
+  sigma_1_ratio <- length(which(abs(s - mean_) <= 1*sd_ ))/size
+  sigma_2_ratio <- length(which(abs(s - mean_) <= 2*sd_ ))/size
+  sigma_3_ratio <- length(which(abs(s - mean_) <= 3*sd_ ))/size
   
-  outliers_minor <- length(outliers$minor)
-  outliers_extream <- length(outliers$extreme)
+  outliers_minor_ratio <- length(outliers$minor) / size
+  outliers_extream_ratio <- length(outliers$extreme) / size
   
   stats <- list("size" = size,
                 "mean" = mean_,
                 "median" = median_,
-                "mean_median" = mean_ - median_,
+                "mean_median_diff" = abs(mean_ - median_) / sd_,
                 "sd" = sd_,
                 "skewness" = skewness_,
                 "kurtosis" = kurtosis_,
-                "outliers_minor" = outliers_minor,
-                "outliers_extream" = outliers_extream,
+                "outliers_minor_ratio" = outliers_minor_ratio,
+                "outliers_extream_ratio" = outliers_extream_ratio,
                 "sigma_1_ratio" = sigma_1_ratio,
                 "sigma_2_ratio" = sigma_2_ratio,
                 "sigma_3_ratio" = sigma_3_ratio)
@@ -57,9 +59,11 @@ find_outliers<-function(data){
   return(outliers)
 }
 
-model_names <- c("rf", "gbm", "svmRadialSigma")
+model_names <- c("rf")#, "gbm", "svmRadialSigma")
 
-round <- "baseline"
+round <- "scaled"
+
+do_scaling <- TRUE
 
 out_dir <- paste("./../out/", round, sep = "")
 models_dir <- paste(out_dir, "/models", sep = "")
@@ -75,7 +79,7 @@ stats_file <- paste(stats_dir, "/statistics.csv", sep = "")
 summary_stats_file <- paste(stats_dir, "/summary_statistics.csv", sep = "")
 power_thresholds_file <- paste(stats_dir, "/power_thresholds.csv", sep = "")
 
-data_dir <- "D:/master/ASDS860 - Thesis/thesis/simulation/data/baseline"
+data_dir <- "D:/master/ASDS860 - Thesis/thesis/simulation/data/scaled"
 data_files_dir <- paste(data_dir, "/files", sep = "")
 dir.create(data_dir, showWarnings = FALSE)
 dir.create(data_files_dir, showWarnings = FALSE)
